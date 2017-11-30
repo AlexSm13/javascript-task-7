74c1d6bd751951463d7559e1ad82eb8172d4d8bd
AlexSm13 <79068122307@yandex.ru>
2017-11-30 17:43:39 +0500

    Мальчишник

--- async.js
+++ async.js
@@ -7,7 +7,38 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
     // асинхронная магия
+    return new Promise(resolve => {
+        let result = [];
+        if (jobs.length === 0) {
+            resolve(result);
+        }
+        let count = 0;
+        for (let i = 0; i < parallelNum; i++) {
+            startJob(jobs[count], count++);
+        }
+
+        function startJob(job, index) {
+            let answer = data => ans(data, index);
+            new Promise((resolveRes, rejectRes) => {
+                job().then(resolveRes, rejectRes);
+                setTimeout(rejectRes, timeout, new Error('Promise timeout'));
+            }).then(answer)
+                .catch(answer);
+        }
+
+        function ans(data, index) {
+            result[index] = data;
+            if (result.length === jobs.length) {
+                resolve(result);
+            }
+
+            if (count < jobs.length) {
+                startJob(jobs[count], count++);
+            }
+        }
+    });
 }
